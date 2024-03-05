e2398016b69fe995580857a7ec4cade7019b6a89
BujjiKeerthi <149158633+BujjiKeerthi@users.noreply.github.com>
2024-03-06 00:01:20 +0530

    Add files via upload

---
+++ task 1/portfolio/js/app.js
@@ -0,0 +1,69 @@
+
+// $(document).ready(function () {
+//     $('.hero-slider').owlCarousel({
+//         loop: true,
+//         autoplay:true,
+//         margin: 0,
+//         nav:true,
+//         items: 1,
+//         smartSpeed:5000,
+//         dots:false,
+//         navText:['PREV','NEXT'],
+//         responsive: {
+//             0: {
+//                 nav:false,
+//             },
+//             768: {
+//                 nav:true,
+                
+//             }
+//         }
+//     })
+// });
+
+
+  
+// $('#projects-slider').owlCarousel({
+//     loop: true,
+//     nav: false,
+//     items: 2,
+//     dots: true,
+//     smartSpeed: 600,
+//     dots:true,
+//     center: true,
+//     autoplay: true,
+//     autoplayTimeout: 4000,
+//     navText:['PREV','NEXT'],
+//     responsive: {
+//         0: {
+//             items: 1,
+//             margin:0,
+//         },
+//         768: {
+//             items: 2,
+//             center:true,
+//             nav:true,
+            
+//         },
+//         1140:{
+//             items:2,
+//             center:true,
+//             nav:true,
+
+//         }
+//     }
+// });
+
+
+// $('.reviews-slider').owlCarousel({
+//     loop: true,
+//     nav: false,
+//     dots: true,
+//     smartSpeed: 900,
+//     items: 1,
+//     margin: 24,
+//     autoplay: true,
+//     autoplayTimeout: 4000,
+    
+
+// });
